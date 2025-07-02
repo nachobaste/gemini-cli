5db185eb3c07d468ddbe464378f5f5fef437dff6
Ignacio <basterrechea@gmail.com>
2025-07-02 15:37:58 -0600

    fixing build variables

--- src/app/projects/[id]/PrintSummary.tsx
+++ src/app/projects/[id]/PrintSummary.tsx
@@ -48,7 +48,7 @@ const PrintSummary: React.FC<PrintSummaryProps> = ({ project, mcdaScore, evaluat
   return (
     <html lang="en">
     <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Project Summary Report</title>
         <style>{`
